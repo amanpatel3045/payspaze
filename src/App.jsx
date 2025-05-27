c8218ebc0580e3fdd6435e66ace23ae40a4b658e
Aman Kumar Patel <107243321+amanpatel3045@users.noreply.github.com>
2025-05-27 14:31:29 +0530

    Created Make Payment Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Button, Box } from '@mui/material';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const [open, setOpen] = useState(false);
+ return (
+    <Box
+      sx={{
+        minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        px: 2,
+      }}
+    >
+    
+      <Button
+        variant="contained"
+        size="large"
+        sx={{ px: 5, py: 1.5, borderRadius: 3 }}
+        onClick={() => setOpen(true)}
+      >
+        Make Payment
+      </Button>
+    </Box>
+  );
 }
+
 
 export default App
